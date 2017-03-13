091330b4eb081543d0c2489ce766f2cceade6731
Liliana Fiore <liliana.fiorela@gmail.com>
2017-03-13 17:59:24 -0500

    v6

--- assets/js/content.js
+++ assets/js/content.js
@@ -4,33 +4,38 @@ var arrHeaders = ["dmngbgr01.thoughtworks.com          |<span>idle          |</s
                   "dmngbgr03.thoughtworks.com          |<span>idle          |</span><span>192.168.0.1    |</span>        var/lib/cruise-agent"];
 
 function addArticle(){
+    var count = arrayArticles.length;
     var sectionContent = document.getElementById("content");
     var article = document.createElement("article");
     var headerArticle = document.createElement("h4");
-    headerArticle.innerHTML = arrHeaders[arrayArticles.length];
+    headerArticle.innerHTML = arrHeaders[count];
     var contentArticle = document.createElement("p");
     contentArticle.innerHTML="+";
-    contentArticle.id="p2";
-    var a2 = document.createElement("a");
-    a2.innerHTML="Specify Resources";
-    contentArticle.appendChild(a2);
-    a2.addEventListener("click", function(e){
+    contentArticle.id="c"+count;
+
+    var addResource = document.createElement("a");
+    addResource.innerHTML="Specify Resources";
+
+    contentArticle.appendChild(addResource);
+
+    addResource.addEventListener("click", function(e){
       e.preventDefault();
-      var z = prompt("¿Que desea agregar?");
 
-      var x = z.split(",");
+      var texto = prompt("¿Que desea agregar?");
 
-      x.forEach(function(e,i){
+      var arrayTexto = texto.split(",");
+
+      arrayTexto.forEach(function(e,i){
         var span = document.createElement("span");
-        span.id="s"+i;
+        span.id="s"+i+count;
         var aRemove = document.createElement("a");
         aRemove.href="#";
         aRemove.setAttribute("class",'glyphicon glyphicon-remove-circle');
         aRemove.addEventListener('click',function(e) {
           e.preventDefault();
-          var padre = document.getElementById("p2");
+          var padre = e.target.parentNode.parentNode;
           var idParent = e.target.parentNode.id;
-          console.log(idParent);
+          console.log(padre);
           padre.removeChild(document.getElementById(idParent));
         });
         span.innerHTML = e;
